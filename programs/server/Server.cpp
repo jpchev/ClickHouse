dd6fb18eaf10c0e79edb55fe6ea5ed0a5ade486a
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-03-20 21:08:47 +0100

    Lint

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -2743,6 +2743,13 @@ try
         systemdNotify("READY=1\n");
 #endif
 
+        auto stop_acme_instance = []{
+#if USE_SSL
+            /// Stop ACME tasks.
+            ACME::Client::instance().shutdown();
+#endif
+        };
+
         SCOPE_EXIT_SAFE({
             LOG_DEBUG(log, "Received termination signal.");
 
@@ -2754,10 +2761,7 @@ try
             main_config_reloader.reset();
             access_control.stopPeriodicReloading();
 
-#if USE_SSL
-            /// Stop ACME tasks.
-            ACME::Client::instance().shutdown();
-#endif
+            stop_acme_instance();
 
             is_cancelled = true;
 
